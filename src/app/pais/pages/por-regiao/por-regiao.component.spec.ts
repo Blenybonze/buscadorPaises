9e5b081da79d9dd98169076e1876c220418b7479
blenybonze <blenybonze@hotmail.com>
2022-12-30 08:10:15 -0300

    Criação dos modulos e componentes com CLI e estruturação dos diretórios

---
+++ src/app/pais/pages/por-regiao/por-regiao.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PorRegiaoComponent } from './por-regiao.component';
+
+describe('PorRegiaoComponent', () => {
+  let component: PorRegiaoComponent;
+  let fixture: ComponentFixture<PorRegiaoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PorRegiaoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PorRegiaoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pais/pages/por-regiao/por-regiao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-por-regiao',
+  templateUrl: './por-regiao.component.html',
+  styleUrls: ['./por-regiao.component.css']
+})
+export class PorRegiaoComponent {
+
+}
